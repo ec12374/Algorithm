e5b12f7013587f4dae999cd115e73faefbd0fb71
Tushar <tusharverma171193@gmail.com>
2020-08-16 00:23:35 +0530

    Create ExperienceCompare.java

---
+++ ComparatorAndComparable/com/tus/compare/ExperienceCompare.java
@@ -0,0 +1,10 @@
+import java.util.Comparator;
+
+public class ExperienceCompare implements Comparator<Employee> {
+
+	public int compare(Employee e1, Employee e2) {
+
+		return (int) (e1.getExperience() - e2.getExperience());
+	}
+
+}
